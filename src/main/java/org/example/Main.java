7dde1556798638e281d6343b49ffa7019be37a71
QWang00 <qiujuanwang87@gmail.com>
2024-07-16 15:54:06 +0100

    feat: create new project

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,27 @@
+package org.example;
+
+
+public class Main {
+    public static void main(String[] args) {
+        String greet = "Hello QJ";
+        System.out.println(greet);
+        int birthYear = 1987;
+        System.out.println(birthYear);
+        double pi = 3.14;
+        System.out.println(pi);
+        boolean learnJava = true;
+        System.out.println(learnJava);
+        String favouriteFilm = "Titanic";
+        System.out.println("My favourite film is " + favouriteFilm);
+        favouriteFilm = "Hannah Montana: The Movie";
+        System.out.println("My favourite film is " + favouriteFilm);
+        
+
+
+
+    }
+    private static void printGreeting(String name){
+        System.out.println("Hello, "+ name);
+    }
+
+}
